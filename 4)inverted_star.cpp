4a91184894d8e70a2abbd57d4ccb0d27ec7369c7
ishaankukade <141508219+ishaankukade@users.noreply.github.com>
2024-08-19 00:12:14 +0530

    Add files via upload

---
+++ 4)inverted_star.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"Enter your choice: ";
+    cin>>n;
+
+    for(i=1;i<=5;i++)
+    {
+        for(j=5;j>=i;j--)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+/*
+Enter your choice: 5
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+*/
